ff2d5d1a7fd0fb80398eac2dab826cd4e591ae74
Luís Henrique <165834056+Passarin1@users.noreply.github.com>
2025-02-13 20:48:32 +0000

    Add - Nome

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import roteadorUsuario from "/src/routes/usuario.js";
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+
+const app = express();                        // Instancia o Express
+const port = 3000;                            // Define a porta
+
+app.use(express.json());                      // Middleware para parsing JSON
+
+// Rota raiz
+app.get("/", (req, res) => {
+  console.log("Rota GET/ solicitada");
+  res.json({
+    nome: "Luis Henrique",                   // Substitua pelo seu nome
+  });
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Serviço escutando na porta: ${port}`);
+});
+app.use(roteadorUsuario);
